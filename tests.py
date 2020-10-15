6e32e809e207efc361292638ba06562c4562a07f
Yonny Clinton <yonnyclinton@BOMC9081968.local>
2020-10-14 19:16:34 -0500

    with circleci

---
+++ tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestServices(unittest.TestCase):
+    def postTest(self):
+        pass
+
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
